b7c38ee0be3ae794ce267e1ca56d94da631dced5
Mohitbishukarma <bkmohit6@gmail.com>
2021-07-15 06:29:22 +0545

    Car game

---
+++ main.js
@@ -0,0 +1,7 @@
+// const score=document.querySelector(".score");
+// const start_pop_up=document.querySelector(".start-pop-up");
+// const gamrArea=document.querySelector(".gameArea");
+// 
+// console.log(score);
+// console.log(start_pop_up);
+// console.log(gamrArea);
